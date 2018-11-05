56e6aff3b0044015b8c76fcaa69af492f36bc9de
Brent Rutledge <Brent@wifi-roaming-128-4-134-60.host.udel.edu>
2018-11-05 12:35:30 -0500

    Add existing file

--- detect_shapes.py
+++ detect_shapes.py
@@ -11,11 +11,20 @@ import cv2
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to the input image")
+ap.add_argument("-s", "--shape", required=True,
+                help="shape to be detected")
+ap.add_argument("-c", "--color,", required=True,
+                 help="color of shape to be detected")
+ap.add_argument("-o", "--output", required=True,
+                 help="name of output file")
 args = vars(ap.parse_args())
 
 # load the image and resize it to a smaller factor so that
-# the shapes can be approximated better
+# the shapes can be approximated better, and load other
+# flags to be used for image searching
 image = cv2.imread(args["image"])
+fshape = args["shape"]
+output = args["output"]
 resized = imutils.resize(image, width=300)
 ratio = image.shape[0] / float(resized.shape[0])
 
@@ -28,28 +37,31 @@ thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
 # find contours in the thresholded image and initialize the
 # shape detector
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
+cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 sd = ShapeDetector()
 
 # loop over the contours
 for c in cnts:
-	# compute the center of the contour, then detect the name of the
-	# shape using only the contour
-	M = cv2.moments(c)
-	cX = int((M["m10"] / M["m00"]) * ratio)
-	cY = int((M["m01"] / M["m00"]) * ratio)
-	shape = sd.detect(c)
+        # compute the center of the contour, then detect the name of the
+        # shape using only the contour
+        M = cv2.moments(c)
+        cX = int((M["m10"] / M["m00"]) * ratio)
+        cY = int((M["m01"] / M["m00"]) * ratio)
+        shape = sd.detect(c)
+        if shape == fshape:
 
-	# multiply the contour (x, y)-coordinates by the resize ratio,
-	# then draw the contours and the name of the shape on the image
-	c = c.astype("float")
-	c *= ratio
-	c = c.astype("int")
-	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-	cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-		0.5, (255, 255, 255), 2)
+                # multiply the contour (x, y)-coordinates by the resize ratio,
+                # then draw the contours and the name of the shape on the image
+                c = c.astype("float")
+                c *= ratio
+                c = c.astype("int")
+                cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+                cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, (255, 255, 255), 2)
 
-	# show the output image
-	cv2.imshow("Image", image)
-	cv2.waitKey(0)
+                # show the output image
+                cv2.imshow(output, image)
+                cv2.waitKey(0)
+
+                        
